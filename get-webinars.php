bb3dba09bdc73513c0b018b9aef927577200618b
antonioortegajr <antonio@mini.local>
2014-12-31 12:34:19 -0800

    Used variables for tokens
    Set the tokens to variables

--- get-webinars.php
+++ get-webinars.php
@@ -1,9 +1,14 @@
 <?php
 
-$uri = 'https://api.citrixonline.com/G2W/rest/organizers/MyOrgID/upcomingWebinars';
+// This can be found in you GotoWebinar account
+$YourOrgID = 'MyOrgID';
+//You will need to create an OAuth Token
+$YourOAuthToken = "MyOAuthToken";
+
+$uri = 'https://api.citrixonline.com/G2W/rest/organizers/'.$YourOrgID . '/upcomingWebinars';
 $ch = curl_init($uri);
 curl_setopt_array($ch, array(
-    CURLOPT_HTTPHEADER  => array('Content-type:application/json', 'Accept: application/json', 'Authorization: OAuth oauth_token=MyOAuthToken'),
+    CURLOPT_HTTPHEADER  => array('Content-type:application/json', 'Accept: application/json', 'Authorization: OAuth oauth_token='. $YourOAuthToken),
     CURLOPT_RETURNTRANSFER  =>true,
     CURLOPT_VERBOSE     => 1
 ));
